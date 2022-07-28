c6d45c24332c811524a4eac744a96014a6225312
vartmaan <vartmaan.gupta@gmail.com>
2022-07-28 06:05:54 +0530

    LCJULY-28-07-2022: ValidAnagram.java

---
+++ src/com/kol/lc/july/twenty/two/ValidAnagram.java
@@ -0,0 +1,43 @@
+package com.kol.lc.july.twenty.two;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        ValidAnagram object = new ValidAnagram();
+        boolean res = object.isAnagram("krishna","anshirk");
+        System.out.println("Is Anagram "+res
+        );
+    }
+
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        for( char ch : s.toCharArray()) {
+
+            map.put(ch, map.getOrDefault(ch,0) + 1);
+        }
+
+
+        for( char ch : t.toCharArray()) {
+
+            int count = map.getOrDefault(ch,0);
+
+            if(count == 0) return false;
+            map.put(ch, map.get(ch) - 1 );
+
+
+
+        }
+
+        return true;
+    }
+}
